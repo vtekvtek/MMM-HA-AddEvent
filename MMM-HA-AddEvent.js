cc8e9981decb81a6ff6fd3317ff2caecf97d66af
V-tec <62038114+vtekvtek@users.noreply.github.com>
2026-03-04 08:52:51 -0500

    Update MMM-HA-AddEvent.js

--- MMM-HA-AddEvent.js
+++ MMM-HA-AddEvent.js
@@ -525,15 +525,15 @@ Module.register("MMM-HA-AddEvent", {
           "1 2 3 4 5 6 7 8 9 0 {bksp}",
           "q w e r t y u i o p",
           "a s d f g h j k l",
-          "{caps} z x c v b n m {shift}",
-          "{space} {clear} {enter}"
+          "{shift} z x c v b n m {caps}",
+          "{clear} {space} {enter}"
         ],
         shift: [
           "! @ # $ % ^ & * ( ) {bksp}",
           "Q W E R T Y U I O P",
           "A S D F G H J K L",
-          "{caps} Z X C V B N M {shift}",
-          "{space} {clear} {enter}"
+          "{shift} Z X C V B N M {caps}",
+          "{clear} {space} {enter}"
         ]
       },
       display: {
